278268e2bd0c4e01c64976104f981bc6ebb6d4b7
olehborys <121348879+olehborys@users.noreply.github.com>
2023-02-22 10:52:16 +0200

    Update script.js

--- script.js
+++ script.js
@@ -35,19 +35,85 @@ function gameLoop() {
 }
 
 const gameLoopId = setInterval(gameLoop, 100);
+const gameContainer = document.getElementById('game-container');
 
-document.addEventListener('keydown', (event) => {
-  if (event.code === 'ArrowLeft') {
-    dx = -20;
-    dy = 0;
-  } else if (event.code === 'ArrowRight') {
-    dx = 20;
-    dy = 0;
-  } else if (event.code === 'ArrowUp') {
-    dx = 0;
-    dy = -20;
-  } else if (event.code === 'ArrowDown') {
-    dx = 0;
-    dy = 20;
+gameContainer.addEventListener('touchstart', handleTouchStart);
+gameContainer.addEventListener('touchmove', handleTouchMove);
+
+let xDown = null;
+let yDown = null;
+
+function handleTouchStart(event) {
+  const firstTouch = event.touches[0];
+  xDown = firstTouch.clientX;
+  yDown = firstTouch.clientY;
+}
+
+function handleTouchMove(event) {
+  if (!xDown || !yDown) {
+    return;
   }
-});
+
+  const xUp = event.touches[0].clientX;
+  const yUp = event.touches[0].clientY;
+
+  const xDiff = xDown - xUp;
+  const yDiff = yDown - yUp;
+
+  if (Math.abs(xDiff) > Math.abs(yDiff)) {
+    // swipe left or right
+    if (xDiff > 0) {
+      dx = -20;
+      dy = 0;
+    } else {
+      dx = 20;
+      dy = 0;
+    }
+  } else {
+    // swipe up or down
+    if (yDiff > 0) {
+      dx = 0;
+      dy = -20;
+    } else {
+      dx = 0;
+      dy = 20;
+    }
+  }
+
+  // reset the touch start position
+  xDown = null;
+  yDown = null;
+}
+
+let pinchStartDistance = null;
+
+gameContainer.addEventListener('gesturestart', handleGestureStart);
+gameContainer.addEventListener('gesturechange', handleGestureChange);
+
+function handleGestureStart(event) {
+  pinchStartDistance = calculatePinchDistance(event);
+}
+
+function handleGestureChange(event) {
+  const pinchDistance = calculatePinchDistance(event);
+
+  if (pinchStartDistance && pinchDistance) {
+    if (pinchDistance > pinchStartDistance) {
+      // zoom in
+      gameContainer.style.transform = `scale(${pinchDistance / pinchStartDistance})`;
+    } else if (pinchDistance < pinchStartDistance) {
+      // zoom out
+      gameContainer.style.transform = `scale(${pinchStartDistance / pinchDistance})`;
+    }
+  }
+}
+
+function calculatePinchDistance(event) {
+  if (event.touches.length === 2) {
+    const touch1 = event.touches[0];
+    const touch2 = event.touches[1];
+    return Math.hypot(touch1.clientX - touch2.clientX, touch1.clientY - touch2.clientY);
+  }
+  return null;
+}
+
