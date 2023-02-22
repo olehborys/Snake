dae80ee4b86f72fdf573d6c65885744a1dbb367c
olehborys <121348879+olehborys@users.noreply.github.com>
2023-02-22 11:02:28 +0200

    Update script.js

--- script.js
+++ script.js
@@ -52,68 +52,95 @@ function handleTouchStart(event) {
 function handleTouchMove(event) {
   if (!xDown || !yDown) {
     return;
-  }
+const canvas = document.getElementById('game-canvas');
+const ctx = canvas.getContext('2d');
 
-  const xUp = event.touches[0].clientX;
-  const yUp = event.touches[0].clientY;
+const TILE_SIZE = 10;
+const BOARD_WIDTH = 40;
+const BOARD_HEIGHT = 40;
+const CANVAS_WIDTH = TILE_SIZE * BOARD_WIDTH;
+const CANVAS_HEIGHT = TILE_SIZE * BOARD_HEIGHT;
 
-  const xDiff = xDown - xUp;
-  const yDiff = yDown - yUp;
+let snake = [{ x: 5, y: 5 }];
+let food = { x: 10, y: 10 };
+let dx = TILE_SIZE;
+let dy = 0;
+let score = 0;
+let gameIsRunning = true;
 
-  if (Math.abs(xDiff) > Math.abs(yDiff)) {
-    // swipe left or right
-    if (xDiff > 0) {
-      dx = -20;
-      dy = 0;
-    } else {
-      dx = 20;
-      dy = 0;
-    }
+function drawSnakePart(snakePart) {
+  ctx.fillStyle = 'green';
+  ctx.fillRect(snakePart.x * TILE_SIZE, snakePart.y * TILE_SIZE, TILE_SIZE, TILE_SIZE);
+}
+
+function drawSnake() {
+  snake.forEach(drawSnakePart);
+}
+
+function moveSnake() {
+  const head = { x: snake[0].x + dx, y: snake[0].y + dy };
+  snake.unshift(head);
+  if (head.x === food.x && head.y === food.y) {
+    score++;
+    generateFood();
   } else {
-    // swipe up or down
-    if (yDiff > 0) {
-      dx = 0;
-      dy = -20;
-    } else {
-      dx = 0;
-      dy = 20;
+    snake.pop();
+  }
+}
+
+function generateFood() {
+  food = {
+    x: Math.floor(Math.random() * BOARD_WIDTH),
+    y: Math.floor(Math.random() * BOARD_HEIGHT)
+  };
+}
+
+function drawFood() {
+  ctx.fillStyle = 'red';
+  ctx.fillRect(food.x * TILE_SIZE, food.y * TILE_SIZE, TILE_SIZE, TILE_SIZE);
+}
+
+function drawScore() {
+  ctx.font = '16px Arial';
+  ctx.fillStyle = 'black';
+  ctx.fillText(`Score: ${score}`, 10, 20);
+}
+
+function clearCanvas() {
+  ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+}
+
+function draw() {
+  clearCanvas();
+  drawSnake();
+  drawFood();
+  drawScore();
+  moveSnake();
+  checkCollision();
+}
+
+function checkCollision() {
+  const head = snake[0];
+  if (head.x < 0 || head.x >= BOARD_WIDTH || head.y < 0 || head.y >= BOARD_HEIGHT) {
+    endGame();
+  }
+  for (let i = 1; i < snake.length; i++) {
+    if (head.x === snake[i].x && head.y === snake[i].y) {
+      endGame();
     }
   }
-
-  // reset the touch start position
-  xDown = null;
-  yDown = null;
 }
 
-let pinchStartDistance = null;
-
-gameContainer.addEventListener('gesturestart', handleGestureStart);
-gameContainer.addEventListener('gesturechange', handleGestureChange);
-
-function handleGestureStart(event) {
-  pinchStartDistance = calculatePinchDistance(event);
+function endGame() {
+  gameIsRunning = false;
+  alert(`Game Over! Your score is ${score}.`);
 }
 
-function handleGestureChange(event) {
-  const pinchDistance = calculatePinchDistance(event);
-
-  if (pinchStartDistance && pinchDistance) {
-    if (pinchDistance > pinchStartDistance) {
-      // zoom in
-      gameContainer.style.transform = `scale(${pinchDistance / pinchStartDistance})`;
-    } else if (pinchDistance < pinchStartDistance) {
-      // zoom out
-      gameContainer.style.transform = `scale(${pinchStartDistance / pinchDistance})`;
-    }
+function handleOrientation(event) {
+  if (!gameIsRunning) {
+    return;
   }
-}
-
-function calculatePinchDistance(event) {
-  if (event.touches.length === 2) {
-    const touch1 = event.touches[0];
-    const touch2 = event.touches[1];
-    return Math.hypot(touch1.clientX - touch2.clientX, touch1.clientY - touch2.clientY);
-  }
-  return null;
-}
-
+  const { gamma, beta } = event;
+  if (gamma > 0) {
+    dx = TILE_SIZE;
+    dy = 
